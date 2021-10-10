6136a11e8e6c5cc9cea4f8954ee78c500fe4dc5c
Farmiana17 <blueruby0717@gmail.com>
2021-10-10 15:11:30 +0900

    商品出品機能実装②

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,19 @@
 FactoryBot.define do
   factory :item do
+    image           {Faker::Lorem.sentence}
     item_name       {Faker::Name.initials(number: 2)}
     item_detail     {'test'}
-    price           {Faker::Internet.price(min: 300, max: 9999999)}
+    price           {Faker::Number.between(from: 300, to: 9999999)}
     category_id     {'1'}
     item_state_id   {'1'}
     delivery_fee_id {'1'}
     prefecture_id   {'1'}
     spend_day_id    {'1'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/image.JPG'), filename: 'image.JPG')
+    end
+    
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe Item, type: :model do
 
   before do
@@ -6,7 +8,7 @@ RSpec.describe Item, type: :model do
 
   context '新規登録できるとき' do
 
-    it 'item_name、item_detail、price、categoly_id、item_state_id、delivery_fee_id、prefecture_id、spend_day_idを入力すれば登録ができる' do
+    it 'image, item_name、item_detail、price、categoly_id、item_state_id、delivery_fee_id、prefecture_id、spend_day_idを入力すれば登録ができる' do
       expect(@item).to be_valid
     end
 
@@ -15,7 +17,7 @@ RSpec.describe Item, type: :model do
   context '新規登録できないとき' do
 
     it 'imageが空では登録できない' do
-      @item.image = ''
+      @item.image = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
     end
@@ -23,13 +25,13 @@ RSpec.describe Item, type: :model do
     it 'item_nameが空では登録できない' do
       @item.item_name = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
     end
     
     it 'item_detailが空では登録できない' do
       @item.item_detail = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Info can't be blank")
+      expect(@item.errors.full_messages).to include("Item detail can't be blank")
     end
 
     it 'priceが空では登録できない' do
@@ -77,13 +79,13 @@ RSpec.describe Item, type: :model do
     it 'item_state_idが選択されていないと登録できない' do
       @item.item_state_id = '0'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Sales state can't be blank")
+      expect(@item.errors.full_messages).to include("Item state can't be blank")
     end
 
     it 'delivery_fee_idが選択されていないと登録できない' do
       @item.delivery_fee_id = '0'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
     end
 
     it 'prefecture_idが選択されていないと登録できない' do
@@ -95,7 +97,7 @@ RSpec.describe Item, type: :model do
     it 'spend_day_idが選択されていないと登録できない' do
       @item.spend_day_id = '0'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Scheduled delively can't be blank")
+      expect(@item.errors.full_messages).to include("Spend day can't be blank")
     end
 
   end
