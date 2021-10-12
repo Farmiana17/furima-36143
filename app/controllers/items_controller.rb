167644032ad6d3dda3bc5c9eece0a13d4bb6289b
Farmiana17 <blueruby0717@gmail.com>
2021-10-12 20:32:04 +0900

    商品一覧表示機能（soldaout表示のみ後で実装）②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,8 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item)
-          .permit(:image, :item_name, :item_detail, :price, :category_id, :item_state_id, :delivery_fee_id, :prefecture_id, :spend_day_id)
+          .permit(:image, :item_name, :item_detail, :price, :category_id, 
+                  :item_state_id, :delivery_fee_id, :prefecture_id, :spend_day_id)
           .merge(user_id: current_user.id)
   end
 
