b52005ca0ef158d86d4c57f87407572d278d23d1
Farmiana17 <blueruby0717@gmail.com>
2021-11-09 01:49:47 +0900

    最終調整

--- spec/factories/purchase_shippings.rb
+++ spec/factories/purchase_shippings.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     address       { '東京1-1' }
     building      { '東京ハイツ101' }
     phone         { '09012345678' }
-    token         { 'tok_abcdefghijk00000000000000000' }
+    token         { "tok_abcdefghijk00000000000000000" }
   end
 end
