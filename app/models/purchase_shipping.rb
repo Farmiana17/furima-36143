0470625e31f21cbf1ac8afe29cf1f0dfb7c07b25
Farmiana17 <blueruby0717@gmail.com>
2021-11-09 10:20:40 +0900

    最終提出（修正版）

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,16 +3,16 @@ class PurchaseShipping
   attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :address, :building, :phone, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :post_number, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :city
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
     validates :address
-    validates :phone, format: { with: /\A\d{10,11}\z/, message: "is too long" }
-    validates :phone, format: { with: /\A\d{10,11}?\z/, message: "is too short" }    
-    validates :phone, numericality: { only_integer: true, message: "is invalid. Input only number" }
-    validates :token
+    validates :phone, format: { with: /\A\d{10,11}\z/, message: "is too long" },
+                      format: { with: /\A\d{10,11}?\z/, message: "is too short" },
+                      numericality: { only_integer: true, message: "is invalid. Input only number" }
   end
 
   def save
